19f8e624b1039609f5facf8d8722904b523c9d9d
Dmitry <boxdima1@gmail.com>
2021-06-24 16:39:54 -0400

    lesson 2

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    title = "магазин"
+    block_items = [
+        {"img": "/static/geekshop/img/product-1.jpg", "header": "Отличный стул", "text": "Расположитесь комфортно."},
+        {"img": "/static/geekshop/img/product-2.jpg", "header": "Стул повышенного качества", "text": "Не оторваться."},
+    ]
+    context = {
+        "title": title,
+        "block_items": block_items
+    }
+    return render(request, "geekshop/index.html", context=context)
 
 
 def contacts(request):
-    return render(request, "contact.html")
+    title = "контакты"
+    locations = [
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+    ]
+    context = {
+        "title": title,
+        "locations": locations
+    }
+    return render(request, "geekshop/contact.html", context=context)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,4 +2,40 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    title = "продукты/каталог"
+    links_menu = [
+        {"href": "products_all", "name": "все"},
+        {"href": "products_home", "name": "дом"},
+        {"href": "products_office", "name": "офис"},
+        {"href": "products_modern", "name": "модерн"},
+        {"href": "products_classic", "name": "классика"}
+    ]
+    products_list = [
+        {
+            "img": "/static/geekshop/img/product-11.jpg",
+            "header": "Стул повышенного качества",
+            "text": "Не оторваться."
+        },
+        {
+            "img": "/static/geekshop/img/product-21.jpg",
+            "header": "Стул повышенного качества",
+            "text": "Не оторваться."
+        },
+        {
+            "img": "/static/geekshop/img/product-31.jpg",
+            "header": "Стул повышенного качества",
+            "text": "Не оторваться."
+        }
+    ]
+    slider_blocks = [
+        {"img": "/static/geekshop/img/controll.jpg"},
+        {"img": "/static/geekshop/img/controll1.jpg"},
+        {"img": "/static/geekshop/img/controll2.jpg"},
+    ]
+    context = {
+        "title": title,
+        "links_menu": links_menu,
+        "products_list": products_list,
+        "slider_blocks": slider_blocks
+    }
+    return render(request, "mainapp/products.html", context=context)
